7b04a50c2ab40e9d348d9ccd2fe2da0159f181b6
Serkan <94819174+Serkangur@users.noreply.github.com>
2021-11-23 08:16:20 +0300

    C# Average
    C# Average

---
+++ sayıalma/sayıalma/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sayıalma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir adet sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            for (int i = 1; i <sayi; i++)
+            {
+                toplam = toplam + i;  
+                
+            }
+            
+            double ortalama = toplam / sayi;
+            if (ortalama>50)
+            {
+                
+                Console.WriteLine("ortalamanız 50 den büyüktür");
+                Console.ReadLine();
+            }
+
+            else
+            {
+               
+                Console.WriteLine("ortalamanız 50 den büyük değildir");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
